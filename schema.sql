3a6f16e9db5e92e809cd0f4042a24b901f0b9bd2
MichelAdrianTorradoRoa <micheltorrado12@gmail.com>
2025-02-21 18:43:13 -0500

    docs: :card_file_box: Update of Database
    by @MichelAdrianTorradoRoa

--- schema.sql
+++ schema.sql
@@ -1,77 +1,50 @@
--- public.admin definition
-
--- Drop table
-
--- DROP TABLE public.admin;
-
-CREATE TABLE public.admin (
-	id int4 NOT NULL,
-	nombres varchar NOT NULL,
-	apellidos varchar NOT NULL,
-	CONSTRAINT admin_pk PRIMARY KEY (id)
-);
-
--- public.reservafecha definition
-
--- Drop table
-
--- DROP TABLE public.reservafecha;
-
-CREATE TABLE public.reservafecha (
-	id int4 NOT NULL,
-	año int4 NOT NULL,
-	mes int4 NOT NULL,
-	dia int4 NOT NULL,
-	hora_inicio time NOT NULL,
-	hora_fin time NOT NULL,
-	CONSTRAINT reservafecha_pk PRIMARY KEY (id)
-);
-
--- public.clientes definition
-
--- Drop table
-
--- DROP TABLE public.clientes;
-
-CREATE TABLE public.clientes (
-	id int4 NOT NULL,
-	nombres varchar NOT NULL,
-	apellidos varchar NOT NULL,
-	dni int4 NULL,
-	telefono varchar NULL,
-	email varchar NOT NULL,
-	CONSTRAINT clientes_pk PRIMARY KEY (id)
+create table admin(
+id SERIAL primary key,
+nombres VARCHAR(50) not null,
+apellidos VARCHAR(50) not null
 );
 
 
--- public.espacio definition
 
--- Drop table
-
--- DROP TABLE public.espacio;
-
-CREATE TABLE public.espacio (
-	id int4 NOT NULL,
-	nombre varchar NOT NULL,
-	tipo varchar NOT NULL,
-	capacidad_maxima int4 NOT NULL,
-	disponibilidad varchar NULL,
-	CONSTRAINT espacio_pk PRIMARY KEY (id),
-	CONSTRAINT espacio_admin_fk FOREIGN KEY (id) REFERENCES public.admin(id)
+create table espacio(
+id SERIAL primary key,
+nombre VARCHAR(25) not null,
+tipo varchar(50) not null,
+capacidad_maxima int not null,
+disponibilidad varchar(50) not null,
+id_Admin int not null,
+foreign key(id_Admin) references admin(id) on delete cascade
 );
 
--- public.reserva definition
-
--- Drop table
-
--- DROP TABLE public.reserva;
-
-CREATE TABLE public.reserva (
-	id int4 NOT NULL,
-	estado varchar NOT NULL,
-	CONSTRAINT reserva_pk PRIMARY KEY (id),
-	CONSTRAINT reserva_clientes_fk FOREIGN KEY (id) REFERENCES public.clientes(id),
-	CONSTRAINT reserva_espacio_fk FOREIGN KEY (id) REFERENCES public.espacio(id),
-	CONSTRAINT reserva_reservafecha_fk FOREIGN KEY (id) REFERENCES public.reservafecha(id)
+create table cliente(
+id SERIAL primary key,
+nombres VARCHAR(50) not null,
+apellidos VARCHAR(50) not null,
+dni int not null,
+telefono varchar(50) not null,
+email varchar(50) not null
 );
+
+
+create table reservafecha(
+id SERIAL primary key,
+year int not null,
+mes int not null,
+dia int not null,
+hora_inicio time not null,
+hora_final time not null
+);
+
+create table reserva(
+id SERIAL primary key,
+estado VARCHAR(25) not null,
+id_Cliente int not null,
+id_Espacio int not null,
+id_ReservaFecha int not null,
+foreign key(id_Cliente) references cliente(id) on delete cascade,
+foreign key(id_Espacio) references espacio(id) on delete cascade,
+foreign key(id_ReservaFecha) references reservafecha(id) on delete cascade
+);
+ALTER TABLE reserva 
+ADD CONSTRAINT unique_reserva UNIQUE (id_Cliente, id_ReservaFecha, id_Espacio);
 
