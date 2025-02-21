2e76e763cc252d4ff2c1e9ab6cd2d7cf39c7f931
juanconde025 <juanconde1309@gmail.com>
2025-02-21 14:50:33 -0500

    feat: :tada: Download spring folder
    By @juanconde025

---
+++ Examen/src/main/java/c3/examen/Examen/ExamenApplication.java
@@ -0,0 +1,13 @@
+package c3.examen.Examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenApplication.class, args);
+	}
+
+}
