00c3abb51d7e731417687773d56f5cb7e9756394
juanconde025 <juanconde1309@gmail.com>
2025-02-21 20:41:34 -0500

    fix repositories

--- ExamConde-Torrado/Examen/src/main/java/c3/examen/Examen/repositories/EspacioRepository.java
+++ ExamConde-Torrado/Examen/src/main/java/c3/examen/Examen/repositories/EspacioRepository.java
@@ -3,11 +3,12 @@ package c3.examen.Examen.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import c3.examen.Examen.entities.espacio;
 
-
+@EnableJpaRepositories
 @Repository
 public interface EspacioRepository extends JpaRepository<espacio,Integer>{
     Optional<espacio> findByNombre(String nombre);
--- ExamConde-Torrado/Examen/src/main/java/c3/examen/Examen/repositories/ReservaRepository.java
+++ ExamConde-Torrado/Examen/src/main/java/c3/examen/Examen/repositories/ReservaRepository.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import c3.examen.Examen.entities.reserva;
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+@EnableJpaRepositories
+@Repository
 public interface ReservaRepository extends JpaRepository<reserva, Integer> {
     List<reserva> findByEstado(String estado);
 }
